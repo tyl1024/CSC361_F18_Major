5f41aba6e6dc96430217aea990bfc7dca53f53bd
Tyler <tm8060@ship.edu>
2018-10-30 21:23:05 -0400

    Adding canyonBunnyMainChapter3 code

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/CanyonBunnyMain.java
@@ -0,0 +1,49 @@
+package com.packtpub.libgdx.canyonbunny.game;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.packtpub.libgdx.canyonbunny.game.WorldController;
+import com.packtpub.libgdx.canyonbunny.game.WorldRenderer;
+
+public class CanyonBunnyMain implements ApplicationListener 
+{
+	private static final String TAG =
+	CanyonBunnyMain.class.getName();
+	private WorldController worldController;
+	private WorldRenderer worldRenderer;
+	
+	@Override 
+	public void create () 
+	{ 
+		
+	}
+	
+	@Override 
+	public void render () 
+	{
+		
+	}
+	
+	@Override 
+	public void resize (int width, int height) 
+	{
+	
+	}
+	
+	@Override 
+	public void pause () 
+	{
+		
+	}
+	
+	@Override 
+	public void resume () 
+	{ 
+		
+	}
+	
+	@Override 
+	public void dispose () 
+	{
+		
+	}
+}
