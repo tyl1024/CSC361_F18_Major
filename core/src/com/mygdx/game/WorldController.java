5ce7fdc7cb3a51801ebd84e7b7cbc4db0458f87a
Tyler <tm8060@ship.edu>
2018-10-30 22:28:52 -0400

    Added Test Sprites to World Controller

--- core/src/com/mygdx/game/WorldController.java
+++ core/src/com/mygdx/game/WorldController.java
@@ -1,7 +1,17 @@
 package com.mygdx.game;
 
+
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+
 public class WorldController 
 	{
+		public Sprite[] testSprites;
+		public int selectedSprite;
+		
 		private static final String TAG =
 		WorldController.class.getName();
 		
@@ -11,7 +21,7 @@ public class WorldController
 		 */
 		public WorldController () 
 		{ 
-			
+			init();
 		}
 		
 		/**
@@ -19,7 +29,53 @@ public class WorldController
 		 */
 		private void init () 
 		{ 
-			
+			initTestObjects();
+		}
+		
+		//Testing object and sprites
+		private void initTestObjects() 
+		{
+			// Create new array for 5 sprites
+			testSprites = new Sprite[5];
+			// Create empty POT-sized Pixmap with 8 bit RGBA pixel data
+			int width = 32;
+			int height = 32;
+			Pixmap pixmap = createProceduralPixmap(width, height);
+			// Create a new texture from pixmap data
+			Texture texture = new Texture(pixmap);
+			// Create new sprites using the just created texture
+			for (int i = 0; i < testSprites.length; i++) {
+			Sprite spr = new Sprite(texture);
+			// Define sprite size to be 1m x 1m in game world
+			spr.setSize(1, 1);
+			// Set origin to sprite's center
+			spr.setOrigin(spr.getWidth() / 2.0f, spr.getHeight() / 2.0f);
+			// Calculate random position for sprite
+			float randomX = MathUtils.random(-2.0f, 2.0f);
+			float randomY = MathUtils.random(-2.0f, 2.0f);
+			spr.setPosition(randomX, randomY);
+			// Put new sprite into array
+			testSprites[i] = spr;
+			}
+			// Set first sprite as selected one
+			selectedSprite = 0;
+		}
+		
+		//Setting pixamp and coloring and drawing rectangle
+		private Pixmap createProceduralPixmap (int width, int height)
+		{
+			Pixmap pixmap = new Pixmap(width, height, Format.RGBA8888);
+			// Fill square with red color at 50% opacity
+			pixmap.setColor(1, 0, 0, 0.5f);
+			pixmap.fill();
+			// Draw a yellow-colored X shape on square
+			pixmap.setColor(1, 1, 0, 1);
+			pixmap.drawLine(0, 0, width, height);
+			pixmap.drawLine(width, 0, 0, height);
+			// Draw a cyan-colored border around square
+			pixmap.setColor(0, 1, 1, 1);
+			pixmap.drawRectangle(0, 0, width, height);
+			return pixmap;
 		}
 
 		/**
@@ -29,6 +85,19 @@ public class WorldController
 		 */
 		public void update (float deltaTime) 
 		{ 
-			
+			updateTestObjects(deltaTime);
 		}
+		
+		//Test sprite rotations
+		private void updateTestObjects(float deltaTime)
+		{
+			// Get current rotation from selected sprite
+			float rotation = testSprites[selectedSprite].getRotation();
+			// Rotate sprite by 90 degrees per second
+			rotation += 90 * deltaTime;
+			// Wrap around at 360 degrees
+			rotation %= 360;
+			// Set new rotation value to selected sprite
+			testSprites[selectedSprite].setRotation(rotation);
+			}
 }
