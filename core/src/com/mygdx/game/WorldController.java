359c106971670310fae1002b3a0783732d2cec60
Tyler <tm8060@ship.edu>
2018-10-30 21:31:58 -0400

    Adding World Controller from Chapter 3

---
+++ core/src/com/mygdx/game/WorldController.java
@@ -0,0 +1,34 @@
+package com.packtpub.libgdx.canyonbunny.game;
+
+public class WorldController 
+	{
+		private static final String TAG =
+		WorldController.class.getName();
+		
+		/**
+		 * Creates an instance of WorldController
+		 * The constructor
+		 */
+		public WorldController () 
+		{ 
+			
+		}
+		
+		/**
+		 * internal init (useful when reseting an object) 
+		 */
+		private void init () 
+		{ 
+			
+		}
+
+		/**
+		 * contains game logic
+		 * called several hundred times per sec
+		 * @param deltaTime
+		 */
+		public void update (float deltaTime) 
+		{ 
+			
+		}
+}
