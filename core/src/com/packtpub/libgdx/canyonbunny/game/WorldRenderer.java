451e03d1a4e0963e04668c32a8f3ef02f6cc0a40
Tyler <tm8060@ship.edu>
2018-10-30 22:38:53 -0400

    adding pg124WorldRenderer from Chapter 3

--- core/src/com/packtpub/libgdx/canyonbunny/game/WorldRenderer.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/WorldRenderer.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 import com.mygdx.game.WorldController;
 import com.packtpub.libgdx.canyonbunny.util.Constants;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class WorldRenderer implements Disposable
 {
@@ -12,23 +13,41 @@ public class WorldRenderer implements Disposable
 	private SpriteBatch batch;
 	private WorldController worldController;
 	
+	//Constructor
 	public WorldRenderer (WorldController worldController) 
 	{
-		
+		this.worldController = worldController;
+		init();
 	}
 	
 	//Method for initialiation
 	private void init () 
 	{
-		
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH,
+		Constants.VIEWPORT_HEIGHT);
+		camera.position.set(0, 0, 0);
+		camera.update();
 	}
 	
 	//method that will contain the logic to define in which order the
 	//game objects are drawn over others.
 	public void render ()
 	{
-		
+		renderTestObjects();
 	}
+	
+	// Testing objects rendering
+	private void renderTestObjects() 
+	{
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		for(Sprite sprite : worldController.testSprites) 
+		{
+			sprite.draw(batch);
+		}
+		batch.end();
+		}
 	
 	/*
 	 * Whenever the screen size is changed, including
@@ -36,7 +55,9 @@ public class WorldRenderer implements Disposable
 	 */
 	public void resize (int width, int height) 
 	{ 
-		
+		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) *
+				width;
+				camera.update();
 	}
 	
 	/*
@@ -46,6 +67,6 @@ public class WorldRenderer implements Disposable
 	@Override 
 	public void dispose () 
 	{ 
-		
+		batch.dispose();
 	}
 }
