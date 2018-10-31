e3f254ea23db7e1be761cfdfcb225944b9015aec
Tyler <tm8060@ship.edu>
2018-10-30 21:06:10 -0400

    Adding Constant Class from Chapter 3

---
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -0,0 +1,22 @@
+package com.packtpub.libgdx.canyonbunny.util;
+/*  Tyler Major
+ *  pg 113 
+ *  9/7/2018
+ *  
+ *  new code added 9/13 from pg162
+ *  
+ *  Edited by Owen Burnham for Assignment 4
+ */
+/**
+ * @author Tyler Major copied from Chapter 3 to set up viewport width
+ * and height
+ */
+public class Constants 
+{
+	// Visible game world is 5 meters wide
+	public static final float VIEWPORT_WIDTH = 5.0f;
+	
+	// Visible game world is 5 meters tall
+	public static final float VIEWPORT_HEIGHT = 5.0f;
+	
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher 
+{
+	public static void main (String[] arg) 
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
 	}
