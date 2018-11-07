03efa31e034a66aaaadf3aa1cd6b6796cd35ac6c
Tyler <tm8060@ship.edu>
2018-11-06 19:51:30 -0500

    Started assembling level class again by getting platforms for santa to jump on ready

--- core/src/com/packtpub/libgdx/canyonbunny/Level.java
+++ core/src/com/packtpub/libgdx/canyonbunny/Level.java
@@ -9,11 +9,11 @@ import com.badlogic.gdx.utils.Array;
 import com.packtpub.libgdx.canyonbunny.Level.BLOCK_TYPE;
 import com.packtpub.libgdx.canyonbunny.game.objects.AbstractGameObject;
 import com.packtpub.libgdx.canyonbunny.game.objects.snowflake;
-import com.packtpub.libgdx.canyonbunny.game.objects.Goal;
 import com.packtpub.libgdx.canyonbunny.game.objects.presents;
 import com.packtpub.libgdx.canyonbunny.game.objects.santa;
 import com.packtpub.libgdx.canyonbunny.game.objects.Mountains;
-import com.packtpub.libgdx.canyonbunny.game.objects.platform;
+import com.packtpub.libgdx.canyonbunny.game.objects.Platform;
+import com.packtpub.libgdx.canyonbunny.game.objects.WaterOverlay;
 import com.packtpub.libgdx.canyonbunny.util.Assets.AssetSanta;
 
 
@@ -26,182 +26,131 @@ import com.packtpub.libgdx.canyonbunny.util.Assets.AssetSanta;
  */
 public class Level 
 {
-	public static final String TAG = Level.class.getName();
-	public Goal goal;
-
-	public enum BLOCK_TYPE 
-	{
+		public static final String TAG = Level.class.getName();
+		public enum BLOCK_TYPE {
 		EMPTY(0, 0, 0), // black
 		PLATFORM(0, 255, 0), // green
 		PLAYER_SPAWNPOINT(255, 255, 255), // white
-		SNOWFLAKE(255, 0, 255), // purple
-		GOAL(236, 56, 56), // red
-		PRESENTS(255, 255, 0); // yellow
+		ITEM_SNOWFLAKE(255, 0, 255), // purple
+		ITEM_PRESENT(255, 255, 0); // yellow
 		private int color;
 		
-		private BLOCK_TYPE (int r, int g, int b) 
+	private BLOCK_TYPE (int r, int g, int b) 
+	{
+		color = r << 24 | g << 16 | b << 8 | 0xff;
+	}
+		public boolean sameColor (int color) 
 		{
-			color = r << 24 | g << 16 | b << 8 | 0xff;
+			return this.color == color;
 		}
-			public boolean sameColor (int color) 
-			{
-				return this.color == color;
-			}
-			public int getColor () 
-			{
-				return color;
-			}
+		public int getColor () 
+		{
+		return color;
 		}
+	}
+		// objects
+		public Array<Platform> platform;
+		
+		// decoration
+		public Mountains mountains;
+		public WaterOverlay waterOverlay;
+		
+		public Level (String filename) 
+		{
+			init(filename);
+		}
+		
+		private void init (String filename)
+		{
+			
 			// objects
-			public Array<Platform> platform;
+			platform = new Array<Platform>();
+			// load image file that represents the level data
+			Pixmap pixmap = new Pixmap(Gdx.files.internal(filename));
+			// scan pixels from top-left to bottom-right
 			
-			// decoration
-			public Mountains mountains;
-			public santa body;
-			public Array<presents> gift;
-			public Array<snowflake> flake;
-			
-			//goal		
-			public Array<Goal> Goal;       
-
-			public Level (String filename) 
+			int lastPixel = -1;
+			for (int pixelY = 0; pixelY < pixmap.getHeight(); pixelY++) 
 			{
-				init(filename);
+			for (int pixelX = 0; pixelX < pixmap.getWidth(); pixelX++) 
+			{
+			AbstractGameObject obj = null;
+			float offsetHeight = 0;
+			// height grows from bottom to top
+			float baseHeight = pixmap.getHeight() - pixelY;
+			// get color of current pixel as 32-bit RGBA value
+			int currentPixel = pixmap.getPixel(pixelX, pixelY);
+			// find matching color value to identify block type at (x,y)
+			// point and create the corresponding game object if there is
+			// a match
+			// empty space
+			if (BLOCK_TYPE.EMPTY.sameColor(currentPixel)) 
+			{
+			// do nothing
 			}
-			private void init (String filename) 
+			// rock
+			else if (BLOCK_TYPE.PLATFORM.sameColor(currentPixel)) 
 			{
-				// objects
-				//Tyler added: player character
-				body = null;
-				
-				// objects
-				platform = new Array<platform>();
-				gift = new Array<presents>();
-				flake = new Array<snowflake>();
-				Goal = new Array<Goal>();
-				// load image file that represents the level data
-				Pixmap pixmap = new Pixmap(Gdx.files.internal(filename));
-				// scan pixels from top-left to bottom-right
-				int lastPixel = -1;
-				for (int pixelY = 0; pixelY < pixmap.getHeight(); pixelY++) {
-				for (int pixelX = 0; pixelX < pixmap.getWidth(); pixelX++) {
-				AbstractGameObject obj = null;
-				float offsetHeight = 0;
-				// height grows from bottom to top
-				float baseHeight = pixmap.getHeight() - pixelY;
-				// get color of current pixel as 32-bit RGBA value
-				int currentPixel = pixmap.getPixel(pixelX, pixelY);
-				// find matching color value to identify block type at (x,y)
-				// point and create the corresponding game object if there is
-				// a match
-				// empty space
-				if (BLOCK_TYPE.EMPTY.sameColor(currentPixel)) 
+				if (lastPixel != currentPixel) 
 				{
-					// do nothing
-				}
-				// platform
-				else if (BLOCK_TYPE.PLATFORM.sameColor(currentPixel))
-				{
-					if (lastPixel != currentPixel) {
-					obj = new platform();
+					obj = new Platform();
 					float heightIncreaseFactor = 0.25f;
 					offsetHeight = -2.5f;
 					obj.position.set(pixelX, baseHeight * obj.dimension.y
 					* heightIncreaseFactor + offsetHeight);
-					platform.add((platform)obj);
+					platform.add((Platform)obj);
 				} 
-				else
-					{
-						platform.get(platform.size - 1).increaseLength(1);
-					}
-				}
-				// player spawn point
-				else if
-				(BLOCK_TYPE.PLAYER_SPAWNPOINT.sameColor(currentPixel))
-				{
-					obj = new santa();
-					offsetHeight = -3.0f;
-					obj.position.set(pixelX,baseHeight * obj.dimension.y +
-							offsetHeight);
-							body = (santa)obj;
-				}
-				// snowflake
-				else if (BLOCK_TYPE.SNOWFLAKE.sameColor(currentPixel)) 
-				{
-					obj = new snowflake();
-					offsetHeight = -1.5f;
-					obj.position.set(pixelX,baseHeight * obj.dimension.y
-					+ offsetHeight);
-					flake.add((snowflake)obj);
-				}
-				// presents
-				else if (BLOCK_TYPE.PRESENTS.sameColor(currentPixel)) 
-				{
-					obj = new presents();
-					offsetHeight = -1.5f;
-					obj.position.set(pixelX,baseHeight * obj.dimension.y
-					+ offsetHeight);
-					gift.add((presents)obj);
-				}
-				// goal
-				else if (BLOCK_TYPE.GOAL.sameColor(currentPixel)) 
-				{
-					obj = new Goal();
-					offsetHeight = -7.0f;
-					obj.position.set(pixelX, baseHeight + offsetHeight);     //Adds end goal to level. Marked in red on png file
-					goal = (Goal)obj;	
-				}
-				// unknown object/pixel color
 				else 
 				{
-				int r = 0xff & (currentPixel >>> 24); //red color channel
-				int g = 0xff & (currentPixel >>> 16); //green color channel
-				int b = 0xff & (currentPixel >>> 8); //blue color channel
-				int a = 0xff & currentPixel; //alpha channel
-				Gdx.app.error(TAG, "Unknown object at x<" + pixelX + "> y<"
-				+ pixelY + ">: r<" + r+ "> g<" + g + "> b<" + b + "> a<" + a + ">");
-				}
-					lastPixel = currentPixel;
+					platform.get(platform.size - 1).increaseLength(1);
 				}
 			}
-				mountains = new Mountains(pixmap.getWidth());
-				mountains.position.set(-1, -1);
-	
-				// free memory
-				pixmap.dispose();
-				Gdx.app.debug(TAG, "level '" + filename + "' loaded");
-				}
-				
-			public void render (SpriteBatch batch) 
+			// player spawn point
+			else if
+			(BLOCK_TYPE.PLAYER_SPAWNPOINT.sameColor(currentPixel)) 
 			{
-				// Draw Mountains
-				mountains.render(batch);
-				// Draw Goal
-				goal.render(batch);           //Tyler added from pg344. Renders end goal which was altered in png file to be red
-				// Draw Rocks
-				for (platform platform : platform)
-				platform.render(batch);
-				// Draw presents
-				for (presents gift : gift)
-				gift.render(batch);
-				// Draw snowflake
-				for (snowflake flake : flake)
-				flake.render(batch);
-				// Draw Player Character
-				body.render(batch);
 			}
+			// feather
+			else if (BLOCK_TYPE.ITEM_SNOWFLAKE.sameColor(currentPixel)) 
+			{
+			}
+			// gold coin
+			else if (BLOCK_TYPE.ITEM_PRESENT.sameColor(currentPixel)) 
+			{
+			}
+			// unknown object/pixel color
+			else {
+			int r = 0xff & (currentPixel >>> 24); //red color channel
+			int g = 0xff & (currentPixel >>> 16); //green color channel
+			int b = 0xff & (currentPixel >>> 8); //blue color channel
+			int a = 0xff & currentPixel; //alpha channel
+			Gdx.app.error(TAG, "Unknown object at x<" + pixelX + "> y<"
+			+ pixelY + ">: r<" + r+ "> g<" + g + "> b<" + b + "> a<" + a + ">");
+			}
+				lastPixel = currentPixel;
+			}
+			}
+			// decoration
+			mountains = new Mountains(pixmap.getWidth());
+			mountains.position.set(-1, -1);
+			waterOverlay = new WaterOverlay(pixmap.getWidth());
+			waterOverlay.position.set(0, -3.75f);
+			// free memory
+			pixmap.dispose();
+			Gdx.app.debug(TAG, "level '" + filename + "' loaded");
+		}
+		
+		public void render (SpriteBatch batch) 
+		{
+			// Draw Mountains
+			mountains.render(batch);
+			// Draw Rocks
+			for (Platform platform : platform)
+			platform.render(batch);
+			// Draw Water Overlay
+			waterOverlay.render(batch);
 			
-			public void update (float deltaTime) 
-			{
-				body.update(deltaTime);
-				for(platform platform : platform)
-				platform.update(deltaTime);
-				for(presents gift : gift)
-				gift.update(deltaTime);
-				for(snowflake flake : flake)
-					flake.update(deltaTime);
-	
-			}
+		}
 }
 		
 			
